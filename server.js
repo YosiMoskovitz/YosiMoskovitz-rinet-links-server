5651a0e2383516ee9592acdbaf38a6e931b97e7b
Yosi Moskovitz <05276ym@gmail.com>
2022-01-02 17:22:46 +0200

    server config

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var allowlist = [
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: req.header('Origin') } // reflect (enable) the requested origin in the CORS response
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
     } else {
         corsOptions = { origin: false } // disable CORS for this request
     }
