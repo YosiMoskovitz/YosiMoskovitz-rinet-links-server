397b5539486e07df2868bcce3526bd299d819b30
Yosi Moskovitz <05276ym@gmail.com>
2022-02-10 17:56:23 +0200

    try 3

--- api/routes/donationes.js
+++ api/routes/donationes.js
@@ -5,9 +5,9 @@ const PATH = "/donationes";
 import controller from '../controllers/donationes.js'
 import Auth from '../middlewares/auth.js'
 
-router.get('/:userId', controller.getUserDonations);
+router.get('/:userId', Auth.checkAuth, controller.getUserDonations);
 router.get('/', Auth.checkAuth, Auth.isAdmin, controller.getAllDonations);
-router.post('/new-donation/:userId', Auth.checkAuth, controller.addDonation);
+router.post('/new-donation/:userId', controller.addDonation);
 
 export default {
     PATH,
