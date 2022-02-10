2dbd2fa02024b9cd73764d263ef212c0f02c57ad
Yosi Moskovitz <05276ym@gmail.com>
2022-02-10 17:59:35 +0200

    try 4

--- server.js
+++ server.js
@@ -101,6 +101,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   if (error instanceof multer.MulterError) res.status(418);
   else res.status(error.status || 500);
+  console.log(error.message)
   res.json({
     message: error.message
   })
