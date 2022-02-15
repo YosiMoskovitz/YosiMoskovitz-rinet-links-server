6f0b3e5c0cece99561be3147a711aebf25943490
Yosi Moskovitz <05276ym@gmail.com>
2022-02-15 12:21:38 +0200

    try 11

--- api/controllers/donationes.js
+++ api/controllers/donationes.js
@@ -9,6 +9,8 @@ var errorObj = {
 export default {
     addDonation: async (req, res) => {
         //only allow req from nedarim plus website
+        console.log("if =", req.headers['x-forwarded-for'] === '18.194.219.73')
+        console.log("req.headers['x-forwarded-for']", req.headers['x-forwarded-for'])
         if (req.headers['x-forwarded-for'] !== '18.194.219.73') throw errorObj
         
         const user = await User.findById(req.params.userId);
